197f1305327569f43c6a908f024ed34dd9c1bdbb
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-01-07 00:03:30 -0500

    re-make backend

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express();
-const port = 3000;
-// base de datos en postgres, username: postgres, password: 00000, port: 5432, db-name: gestint-db
-app.listen(port, () => {
-    console.log('Servidor activo')
-});
